849742ce21d38f4be843464613afbf07fc9adc9b
markymark <smallitle@hotmail.com>
2013-06-08 13:50:04 -0700

    shell, first pass

---
+++ sqsh.js
@@ -0,0 +1,93 @@
+var squint = require( 'squint' )
+  , fs = require( 'fs' )
+  , join = require( 'path' ).join
+  , command = 0
+  , source = __dirname;
+  
+var app = { 
+    execute: function() {
+    app.printHelp(); 
+  }, 
+  executeCommand: function() {
+  
+    console.log( 'executing command on: ' + source );
+  
+    fs.stat( source, function( err, stat ) {
+      
+      var processFile = function( path ) {
+          console.log( 'processing file ' + path ); 
+          fs.readFile( path, function( err, data ) {
+            if (err) throw err;
+            command( data );
+          } );
+        }
+        , processDirectory = function( path ) {
+          console.log( 'processing path ' + path ); 
+          fs.readdir( path, function( err, files ) {
+            if (err) throw err;
+            files.forEach( function( file ) {
+              file = join( path, file );
+              fs.stat( file, function( err, stat ) {
+                if (err) throw err;
+                if (stat && stat.isDirectory()) {
+                  processDirectory( file );
+                }
+                else {
+                  processFile( file );
+                }
+              } );
+            } );
+          } );
+        };
+      
+      if (err) throw err;
+      if (!stat.isDirectory()) {
+        processFile( source );
+      }
+      else {
+        processDirectory( source );
+      }
+    } );
+  },
+  cleanup: function() { 
+    console.log( 'see ya' ); 
+  },
+  printHelp: function() {
+    console.log( '\nUsage: node squint [command]' ); 
+    console.log( '\nOptions:\n  -s, --strip      remove comments\n' );
+  },
+  parseArgs: function( args, done ) {
+  
+    var counter = args.length
+      , decCounter = function() {
+          if (!-counter) {
+            done();
+          }
+        };
+
+    if (!counter) {
+      done();
+      return;
+    }
+    args.forEach( function( arg ) { 
+      switch (arg) {
+        case '-h': case '--help':
+          app.printHelp(); 
+          process.exit( 0 ); 
+          break;
+        case '-s': case '--strip':
+          command = squint.strip_comments;
+          app.execute = app.executeCommand;
+          break;
+        default:
+          source = arg;
+          break;
+      }
+      decCounter();
+    } ); 
+  },
+};
+
+process.on( 'SIGINT', app.cleanup ); 
+app.parseArgs( process.argv.slice(2) ); 
+app.execute(); 
