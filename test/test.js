5d2b394d4096b8b4e3895defc514d257ce33fe21
markymark <smallitle@hotmail.com>
2013-06-08 17:43:09 -0700

    removing include, define and undef. not all directives

--- test/test.js
+++ test/test.js
@@ -1,19 +1,47 @@
 var assert = require( 'assert' )
   , squint = require( 'squint' )
 
-preProDirectives();
+defines();
+includes();
 comments();
 
-function preProDirectives() {
+function defines() {
+  var strip = squint.stripDefines;
+  
+  assert.equal( strip( '#define' ), '' );
+  assert.equal( strip( '#define text text' ), '' );
+  
+  assert.equal( strip( '# define' ), '' );
+  assert.equal( strip( '#	define text text' ), '' );
 
-  var strip = squint.stripPreProDirectives;
+  assert.equal( strip( '#define' ), '' );
+  assert.equal( strip( '#define text \\n text' ), '' );
+  assert.equal( strip( '#define text \	\n text' ), '' );
 
-  // single lines
+  assert.equal( strip( '#	define' ), '' );
+  assert.equal( strip( '# define text \\n text' ), '' );
+  assert.equal( strip( '#	define text \	\n text' ), '' );
+
+  assert.equal( strip( '#undef' ), '' );
+  assert.equal( strip( '# undef		\n' ), '' );
+}
+
+function includes() {
+
+  var strip = squint.stripIncludes;
+
   assert.equal( strip( '#include' ), '' );
   assert.equal( strip( '#include\n' ), '' );
   assert.equal( strip( '#include "text.h"' ), '' );
   assert.equal( strip( '#include "text.h"\n' ), '' );
+  
+  assert.equal( strip( '# include' ), '' );
+  assert.equal( strip( '#	include\n' ), '' );
+  assert.equal( strip( '# 	include "text.h"	' ), '' );
+  assert.equal( strip( '#  include "text.h"	\n' ), '' );
+}
 
+/*
   // multiple lines  
   assert.equal( strip( '#define text \\ntext' ), '' );
   assert.equal( strip( '#define text \\ntext\n' ), '' );
@@ -21,7 +49,7 @@ function preProDirectives() {
   assert.equal( strip( '#define text \\ntext\\ntext\n' ), '' );
   assert.equal( strip( '#define text \	\ntext\ \ntext' ), '' );
   assert.equal( strip( '#define text \ 	\ntext\ \ntext		\n' ), '' );
-}
+*/
 
 function comments() {
 
@@ -33,12 +61,12 @@ function comments() {
   assert.equal( strip( '/* text; * /' ), '/* text; * /' );
   assert.equal( strip( '/ * text; */' ), '/ * text; */' );
 
-  assert.equal( squint.stripComments( '// text \n' ), '' );
-  assert.equal( squint.stripComments( '// text \n\n' ), '\n' );
-  assert.equal( squint.stripComments( '// text \ntext // text\n' ), 'text ' );
-  assert.equal( squint.stripComments( '// text ' ), '' );
-  assert.equal( squint.stripComments( '// text \ntext // text' ), 'text ' );
+  assert.equal( strip( '// text \n' ), '' );
+  assert.equal( strip( '// text \n\n' ), '\n' );
+  assert.equal( strip( '// text \ntext // text\n' ), 'text ' );
+  assert.equal( strip( '// text ' ), '' );
+  assert.equal( strip( '// text \ntext // text' ), 'text ' );
   
-  assert.equal( squint.stripComments( '/* text */' ), '' ); 
-  assert.equal( squint.stripComments( '/* text */text /*text*/' ), 'text ' ); 
+  assert.equal( strip( '/* text */' ), '' ); 
+  assert.equal( strip( '/* text */text /*text*/' ), 'text ' ); 
 }
