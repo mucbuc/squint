72d89d115665df4c2d2c74126874f87dd499ad2c
markymark <smallitle@hotmail.com>
2013-06-10 12:50:17 -0700

    improved parser test

--- test/parser.js
+++ test/parser.js
@@ -7,101 +7,70 @@ testScopes();
 testStatments();
 
 function testNested() {
-  var parser = new Parser( this );
-  
-  this.emit = function( event, name ) {
-    assert.equal( event, 'open' );
-    assert.equal( name, 'a' );
-    this.emit = function( event, name ) {
-      assert.equal( event, 'open' ); 
-      assert.equal( name, 'b' ); 
-      this.emit = function( event, name ) {
-        assert.equal( event, 'statement' );
-        assert.equal( name, 'c' ); 
-        this.emit = function( event ) {
-          assert.equal( event, 'close' );
-          this.emit = function( event ) {
-            assert.equal( event, 'close' );
-            this.emit = assert.fail;
-          }; 
-        };
-      };
-    };
-  }; 
+  var parser = new TestParser();
+
+  parser.expect( 'open', 'a' );
+  parser.expect( 'open', 'b' );
+  parser.expect( 'statement', 'c' );
+  parser.expect( 'close' );
+  parser.expect( 'close' );
 
   parser.process( 'a { b { c; } }' );
 };
 
 function testInterleaved() { 
-  var parser = new Parser( this );
+  var parser = new TestParser();
   
-  this.emit = function( event, name ) {
-    assert.equal( event, 'open' );
-    assert.equal( name, 'a' );
-    this.emit = function( event, name ) {
-      assert.equal( event, 'statement' );
-      assert.equal( name, 'b' );
-      this.emit = function( event ) {
-        assert.equal( event, 'close' );
-        this.emit = assert.fail;
-      };
-    }
-  }; 
-  
+  parser.expect( 'open', 'a' );
+  parser.expect( 'statement','b' );
+  parser.expect( 'close' );
+   
   parser.process( 'a{ b; }' );
 }
 
 function testScopes() {
-  var parser = new Parser( this );
+  var parser = new TestParser();
   
-  this.emit = function( event, name ) {
-    assert.equal( event, 'open' );
-    assert.equal( name, 'a' );
-    this.emit = function( event ) {
-      assert.equal( event, 'close' );
-      this.emit = function( event, name ) {
-        assert.equal( event, 'open' );
-        assert.equal( name, 'b' );
-        this.emit = function( event ) { 
-          assert.equal( event, 'close' );
-          this.emit = function( event, name ) { 
-            assert.equal( event, 'open' );
-            assert.equal( name, 'c' );
-            this.emit = function( event ) { 
-              assert.equal( event, 'close' );
-              this.emit = assert.fail;
-            };
-          };
-        };
-      };
-    };
-  };
-  
+  parser.expect( 'open', 'a' ); 
+  parser.expect( 'close' );
+  parser.expect( 'open', 'b' );
+  parser.expect( 'close' );
+  parser.expect( 'open', 'c' );
+  parser.expect( 'close' );
+ 
   parser.process( 'a{} b{} c{}' );
 }
 
 function testStatments() {
-  var parser = new Parser( this );
+  var parser = new TestParser();
 
-  this.emit = function( event, source ) {
-    assert.equal( event, 'statement' );
-    assert.equal( source, '1' );
-  
-    this.emit = function( event, source ) {
-      assert.equal( event, 'statement' );
-      assert.equal( source, '2' );
-
-      this.emit = function( event, source ) {
-        assert.equal( event, 'statement' );
-        assert.equal( source, '3' );
-        this.emit = function( event, source ) {
-          assert.equal( event, 'statement' );
-          assert.equal( source, "" );
-          this.emit = assert.fail;
-        };
-      }   
-    }   
-  };
-
+  parser.expect( 'statement', '1' );
+  parser.expect( 'statement', '2' );
+  parser.expect( 'statement', '3' );
+  parser.expect( 'statement' );
+ 
   parser.process( '1; 2; 3;;' );
 }
+
+function TestParser() {
+  var expectations = [];
+  
+  Parser.call( this, this );
+  
+  this.emit = function( event, code ) {
+
+    var expectation = expectations[0];
+    expectations.splice( 0, 1 );
+    
+    assert.equal( event, expectation.event );
+    if (expectation.code != undefined) {
+      assert.equal( code, expectation.code );
+    }
+  };
+
+  this.expect = function( event, code ) {
+    expectations.push( { event: event, code: code } );
+  }; 
+};
+
+TestParser.prototype = new Parser();
