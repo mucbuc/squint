9a778d9ebba135455883a0c9b60bc810f7411067
markymark <smallitle@hotmail.com>
2013-06-10 21:07:14 -0700

    builder test

---
+++ test/builder.js
@@ -0,0 +1,31 @@
+var assert = require( 'assert' )
+  , events = require( 'events' )
+  , Parser = require( 'parser' ).Parser
+  , Builder = require( 'builder' ).Builder;
+  
+basics();
+
+function basics() {
+  var builder = new TestBuilder();
+}
+
+function TestBuilder() {
+  var emitter = new events.EventEmitter()
+    , parser = new Parser( emitter )
+    , builder = new Builder( emitter );
+  
+  emitter.on( 'open', function( code ) {
+    builder.document = builder.document.concat( 'code ' + '{' );
+  } );
+  
+  emitter.on( 'close', function( code ) {
+    builder.document = builder.document.concat( '}' );
+  } );
+  
+  parser.process( 'text { text; }' ); 
+  
+  process.on( 'exit', function() { 
+    console.log( builder.document );
+  } );
+}
+
