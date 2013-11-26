f745b42c573839b47ac2a483cd93c7fcbcfd9b70
markymark <smallitle@hotmail.com>
2013-11-25 21:16:35 -0800

    add declare and define

--- src/squint.js
+++ src/squint.js
@@ -8,6 +8,7 @@ var assert = require( 'assert' )
   , undefine = new RegExp( '#.*undef.*\n?', 'mg' )
   , stringLiteral = new RegExp( '".*?([^\\\\]")', 'g' )
   , arrayInitBlock = RegExp( '\\s*=.*?;', 'g' )
+  , Parser = require( 'Parser' )
   , Forwarder = require( './builder/forwarder').Forwarder;
   
 assert( typeof Forwarder !== 'undefined' );
@@ -45,10 +46,40 @@ exports.forward = function( code, done ) {
       , parser = new Parser()
       , builder = new Forwarder( emitter );
   
+  console.log( 'forward', code );
+
   emitter.on( 'forward declare', function( types ) {
-    done( types );
+    done( types.toString() );
   } );
 
   parser.process( code, emitter );
 }
-	
+
+exports.declare = function( code, done ) { 
+    var emitter = new events.EventEmitter()
+      , parser = new Parser()
+      , builder = new Declarer( emitter );
+  
+  console.log( 'declare', code );
+
+  emitter.on( 'type decalartions', function( types ) {
+    done( types.toString() );
+  } );
+
+  parser.process( code, emitter );
+}
+
+exports.define = function( code, done ) { 
+    var emitter = new events.EventEmitter()
+      , parser = new Parser()
+      , builder = new Definer( emitter );
+  
+  console.log( 'define', code );
+
+  emitter.on( 'type definition', function( defs ) {
+    done( defs.toString() );
+  } );
+
+  parser.process( code, emitter );
+}
+  
