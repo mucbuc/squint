720c2054e460b82c179ba4205642b6efb764f3dc
markymark <smallitle@hotmail.com>
2013-11-30 08:51:19 -0800

    test stubs

--- test/type.js
+++ test/type.js
@@ -6,6 +6,39 @@ var assert = require( 'assert' )
 assert( typeof Type !== 'undefined' );
 
 testNamespace();
+testType(); 
+testFunction();
+
+function testFunction() {
+	test( defineFunction );
+
+	function defineFunction(emitter, parser) {
+		emitter.expectNot( 'define namespace' );
+		emitter.expectNot( 'define type' );
+
+		emitter.expect( 'define function', { name: 'void foo()', code: 'do something' } );
+		parser.process( 'void foo() { do something }', emitter );
+		
+		emitter.expect( 'define function', { name: 'void fool()', code: 'do nothing' } );
+		parser.process( 'void fool() { do nothing }', emitter );
+	}
+}
+
+function testType() {
+
+	test( defineType ); 
+	
+	function defineType(emitter, parser) {
+		emitter.expectNot( 'define namespace' );
+		emitter.expectNot( 'define function' );
+
+		emitter.expect( 'define type', { name: 'struct hello', code: 'unsigned world;' } );
+		parser.process( 'struct hello { unsigned world; }', emitter );
+	
+		emitter.expect( 'define type', { name: 'struct cya', code: 'yes' } );
+		parser.process( 'struct cya { yes}', emitter );
+	}
+}
 
 function testNamespace() {
 
