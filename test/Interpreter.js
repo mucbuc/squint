5dd46f3173b52f145876ed18d28be4b609e32f1b
markymark <smallitle@hotmail.com>
2013-12-02 05:25:47 -0800

    merge test case fail

--- test/Interpreter.js
+++ test/Interpreter.js
@@ -10,10 +10,23 @@ testInterpreter();
 function testInterpreter() {
 
 	Base.test( interpretDeclarationsAndDefinitions );
+	Base.test( interpretMergeProduct );
+
+	function interpretMergeProduct(emitter, parser) {
+		var interpreter = new Interpreter();
+		interpreter.process( 'struct hello;', emitter );
+		interpreter.process( 'struct world;', emitter );
+
+		assert.deepEqual( interpreter.declarations, { 
+				namespaces:{}, 
+				types:{ 'struct hello': 'undefined', 'struct world': 'undefined' }, 
+				functions:{} 
+			} );
+	}
 
 	function interpretDeclarationsAndDefinitions(emitter, parser) {
 		var interpreter = new Interpreter();
-		interpreter.process( 'struct hello;', emitter ); 
+		interpreter.process( 'struct hello;', emitter );
 	
 		assert.deepEqual( interpreter.declarations, { 
 				namespaces:{}, 
