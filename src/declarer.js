0d92fc291812beaef1b6eecb7f0ff9175da3dbda
markymark <smallitle@hotmail.com>
2013-12-03 08:56:07 -0800

    pass test

--- src/declarer.js
+++ src/declarer.js
@@ -8,31 +8,47 @@
 
 var assert = require( 'assert' )
   , events = require( 'events' )
-  , Parser = require( './parser' ).Parser;
+  , Parser = require( './parser' ).Parser
+  , Scoper = require( './scoper' ).Scoper;
 
 function Declarer(emitter) {
 
-	Parser.call( this, emitter );
+	Scoper.call( this, emitter );
 
-	emitter.on( 'statement', function(code) {
-		
-		if (isType(code)) {
-			emitter.emit( 'declare type', code );
-		}
-		else if (isFunctionDeclaration(code)) { 
-			emitter.emit( 'declare function', code );
-		}	
-
-		function isFunctionDeclaration(code) {
-			return code.search( /(\w*\s+)*\w*\s*\(.*\)\s*/ ) == 0;
-		}
-
-		function isType() {
-			return code.search( /(struct|class)/ ) != -1; 
-		}
+	emitter.on( 'open scope', function( code ) {
+		declare(code); 
 	} );
+	
+	emitter.on( 'end', function( code ) {
+		declare(code);
+	} );
+
+	function declare(code) {
+		
+		var sub = new events.EventEmitter()
+		  , parser = new Parser( sub );
+
+		sub.on( 'statement', function(code) {
+			if (isType(code)) {
+				emitter.emit( 'declare type', code );
+			}
+			else if (isFunctionDeclaration(code)) { 
+				emitter.emit( 'declare function', code );
+			}	
+
+			function isFunctionDeclaration(code) {
+				return code.search( /(\w*\s+)*\w*\s*\(.*\)\s*/ ) == 0;
+			}
+
+			function isType() {
+				return code.search( /(struct|class)/ ) != -1; 
+			}
+		} );
+
+		parser.process( code );
+	}
 }
 
-Declarer.prototype = new Parser();
+Declarer.prototype = new Scoper();
 
 exports.Declarer = Declarer;
