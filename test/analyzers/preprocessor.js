4253918248658f1946fa88015c4235bd9196a906
markymark <smallitle@hotmail.com>
2014-05-24 18:26:31 -0700

    multiline preprocessor

--- test/analyzers/preprocessor.js
+++ test/analyzers/preprocessor.js
@@ -1,15 +1,21 @@
 #!/usr/bin/env node
 
-var assert = require( 'assert' )
+var assert = require( 'chai' ).assert
   , Base = require( '../base' ).Base
   , events = require( 'events' )
   , Preprocessor = require( '../../src/analyzers/preprocessor' ).Preprocessor; 
 
 assert( typeof Preprocessor !== 'undefined' );
 
-test( preprocessor );
+test( preprocessorSingleLine );
+test( preprocessorMultiLine );
 
-function preprocessor(emitter, parser) {
+function preprocessorMultiLine(emitter, parser) {
+	emitter.expect( 'preprocess', '#define hello hello\\n\hello\n' );
+	parser.process( '#define hello hello\\nhello\n' ); 
+}
+
+function preprocessorSingleLine(emitter, parser) {
 	emitter.expect( 'preprocess', '#define hello hello\n' );
 	parser.process( '#define hello hello\n' ); 
 }
--- test/test.js
+++ test/test.js
@@ -15,6 +15,7 @@ function runTest() {
   cp.fork( './analyzers/definer' ); 
   cp.fork( './analyzers/declarer' );
   cp.fork( './analyzers/template' );
+  cp.fork( './analyzers/preprocessor' );
   cp.fork( './factory' );
   cp.fork( './builder' );
   cp.fork( './squint' );
