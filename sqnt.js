e85ec142438598df8df473d1bc893537e4488774
markymark <smallitle@hotmail.com>
2014-05-25 22:15:58 -0700

    removed typedef from model

--- sqnt.js
+++ sqnt.js
@@ -9,8 +9,8 @@ if (process.argv.length == 3) {
 		
 		var cleaned = squint.stripComments( data.toString() );
 		//cleaned = squint.stripDefines( cleaned );
-		cleaned = squint.stripPreprocessor( cleaned ); 
-		cleaned = squint.stripStrings( cleaned );
+		//cleaned = squint.stripPreprocessor( cleaned ); 
+		//cleaned = squint.stripStrings( cleaned );
 		//cleaned = squint.stripTypedefs( cleaned ); 
 		squint.compile( cleaned, function( model ) {
   			console.log( model );
