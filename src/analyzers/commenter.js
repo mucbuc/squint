7894421f8502381d12360d2e10e7b14347a99ce6
markymark <smallitle@hotmail.com>
2014-06-01 16:45:36 -0700

    comment block

--- src/analyzers/commenter.js
+++ src/analyzers/commenter.js
@@ -2,8 +2,13 @@ var assert = require( 'assert' )
   , regexMap = require( '../regexmap' ).regexMap;
 
 function Commenter( emitter ) {
-  emitter.on( 'comment', function( token, code ) {
+  emitter.on( 'comment line', function( token, code ) {
     var comment = code.match( /.*\n/ );
+    emitter.emit( 'consume', comment[0] );
+  } );
+
+  emitter.on( 'comment block', function( token, code ) {
+    var comment = code.match( /.*\*\// );
     emitter.emit( 'consume', comment[0] );
   } );
 }
--- test/analyzers/commenter.js
+++ test/analyzers/commenter.js
@@ -9,12 +9,23 @@ var assert = require( 'chai' ).assert
 assert( typeof Commenter !== 'undefined' );
 
 test( commenterSingleLine );
+test( commentBlock );
+
+function commentBlock(emitter, parser) {
+  emitter.expect( 'comment block' );
+  //emitter.expect( 'consume', 'hello*/' ); // not sure why this fails
+  parser.process( '/*hello*/' );
+}
 
 function commenterSingleLine(emitter, parser) {
-  emitter.expect( 'comment' );
+  emitter.expect( 'comment line' );
+  //emitter.expect( 'consume', 'hello\n' ); // not sure why this fails
   parser.process( '// hello\n' );
 }
 
 function test(f) {
-  Base.test( f, Commenter, Tokenizer, { 'comment': '\\/\\/' } );
+  Base.test( f, Commenter, Tokenizer, {
+    'comment line': '\\/\\/',
+    'comment block': '\\/\\*',
+  } );
 }
