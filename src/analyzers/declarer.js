5d182126767cf76a3da29d9456497a3205cee5c2
markymark <smallitle@hotmail.com>
2014-08-11 21:54:32 -0700

    fixed declarer test

--- src/analyzers/declarer.js
+++ src/analyzers/declarer.js
@@ -1,6 +1,6 @@
 var assert = require( 'assert' )
-  , Tokenizer = require( 'mucbuc-jsthree' ).Tokenizer
-  , regexMap = require( '../regexmap' ).regexMap;
+  , regexMap = require( '../regexmap' ).regexMap
+  , fluke = require( 'flukejs' );
 
 function Declarer(emitter) {
 
@@ -8,33 +8,31 @@ function Declarer(emitter) {
     declare(code);
   } );
 
-  emitter.on( 'end', function( code ) {
+  emitter.on( 'end', function( bullshit, code ) {
     declare(code);
   } );
 
   function declare(code) {
+    fluke.splitAll( code, function(type, lhs, rhs, token) {
+        if (type == 'statement') {
+          if (isType(lhs)) {
+            emitter.emit( 'declare type', lhs );
+          }
+          else if (isFunctionDeclaration(lhs)) {
+            emitter.emit( 'declare function', lhs );
+          }
 
-    var sub = Object.create( emitter.constructor.prototype )
-      , tokenizer = new Tokenizer( sub, { 'statement': ';' } );
+          function isFunctionDeclaration(code) {
+            return code.search( regexMap.functionDeclare ) == 0;
+          }
 
-    sub.on( 'statement', function(code) {
-      if (isType(code)) {
-        emitter.emit( 'declare type', code );
-      }
-      else if (isFunctionDeclaration(code)) {
-        emitter.emit( 'declare function', code );
-      }
-
-      function isFunctionDeclaration(code) {
-        return code.search( regexMap.functionDeclare ) == 0;
-      }
-
-      function isType() {
-        return code.search( regexMap.typeDeclare ) != -1;
-      }
-    } );
-
-    tokenizer.process( code );
+          function isType(code) {
+            return code.search( regexMap.typeDeclare ) != -1;
+          }
+        }
+      }, 
+      { 'statement': ';' } 
+    );
   }
 }
 
--- test/analyzers/declarer.js
+++ test/analyzers/declarer.js
@@ -13,29 +13,40 @@ test( declareNot );
 test( ignoreSubScopes );
 
 function ignoreSubScopes(emitter, parser) {
+  var declarer = new Declarer( emitter ); 
   emitter.expectNot( 'declare type' );
-  parser.process( 'namespace { struct hello; }' );
+  parser.process( 'namespace { struct hello; }', defaultMap() );
 }
 
 function declareNot(emitter, parser) {
+  var declarer = new Declarer( emitter );
   emitter.expectNot( 'declare function' );
   parser.process( 'bla bla;' );
-  parser.process( 'bla += bla();' );
+  parser.process( 'bla += bla();', defaultMap() );
 }
 
 function declareFunction( emitter, parser ) {
+  var declarer = new Declarer( emitter );
   emitter.expectNot( 'define function' );
   emitter.expect( 'declare function', 'void foo()' );
-  parser.process( 'void foo();' );
+  parser.process( 'void foo();', defaultMap() );
 }
 
 function declareType( emitter, parser ) {
+  var declarer = new Declarer( emitter );
   emitter.expectNot( 'define type' );
-
   emitter.expect( 'declare type', 'struct bla' );
-  parser.process( 'struct bla;' );
+  parser.process( 'struct bla;', defaultMap() );
+}
+
+function defaultMap() {
+  return {
+    //'statement': ';',
+    'open': '{',
+    'close': '}',
+  };
 }
 
 function test(f) {
-  Base.test( f, Declarer, Scoper );
+  Base.test( f, Scoper );
 }
