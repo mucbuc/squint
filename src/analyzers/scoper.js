85988da2916f77c1976ccc39672b3ccf4cbfa554
markymark <smallitle@hotmail.com>
2014-08-15 23:03:28 -0700

    rework scoper logic

--- src/analyzers/scoper.js
+++ src/analyzers/scoper.js
@@ -1,25 +1,13 @@
-var assert = require( 'assert' )
-  , fluke = require( 'flukejs' );
+var assert = require( 'assert' );
 
-assert( typeof fluke !== 'undefined' );
-
-function Scoper( emitter, openToken, closeToken ) {
+function Scoper( emitter, cb ) {
 
   var instance = this
     , depth = 0
-    , content = ''
-    , _rules = {};
+    , content = '';
 
   if (typeof emitter === 'undefined')
     return;
-
-  this.process = function( code, rules ) {
-    if (typeof rules === 'undefined') {
-      rules = initMap( openToken, closeToken ); 
-    }
-    _rules = rules;
-    splitNext( code, rules );
-  }; 
 
   emitter.on( 'open', function(code, src, token) {
     if (!depth)
@@ -27,7 +15,7 @@ function Scoper( emitter, openToken, closeToken ) {
     else
       content += code.trim() + token;
     ++depth;
-    splitNext( src, _rules ); 
+    cb( src );
   } );
 
   emitter.on( 'close', function(code, src, token) {
@@ -40,71 +28,13 @@ function Scoper( emitter, openToken, closeToken ) {
     else {
       content += code.trim() + token;
     }
-    splitNext( src, _rules ); 
+    cb( src ); 
   } );
-  
-  function splitNext( code, rules ) {
-    fluke.splitNext( code, function( type, lhs, rhs, token ) { 
-        emitter.emit( type, lhs, rhs, token );
-      } 
-      , rules );
-  }
 
-  // this.step = function( code, rules ) {
-  //   if (typeof rules === 'undefined') {
-  //     rules = initMap( openToken, closeToken ); 
-  //   }
-
-  //   fluke.splitNext( code, function( type, lhs, rhs, token ) {
-  //       emitter.emit( type, lhs, rhs, token );
-  //     }, rules );
-  // };
-
-
-
-  // emitter.on( 'comment', function() {
-  //   emitter.remove( 'open' ..
-  //   emitter.remove( 'close' ... )
-  // });
-
-  /* hackybacky this belongs on a higher level, what if content should be ignored?
-    Or what if new token gets added? same with two cases above
-
-  *Response: the content can optionally be ignored at the higher level. content doesn't
-  refer to what is in the result but what is in the current scope. 
-  The problem here is that Scoper needs to know about tags like "statement" and any added ones 
-  in order to correctly parse the content. I need to splitNext with "{}" (this is assuming that 
-  Preprocessor directives and comments have been removed).
-  */
-  emitter.on( 'statement', function(code) {
+  emitter.on( 'statement', function(code, src) {
     content += code.trim() + ';';
+    cb(src);
   } );
-
-  function initMap() {
-    var result = {};
-    if (typeof openToken === 'undefined')
-      openToken = '{';
-    result['open'] = openToken;
-
-    if (typeof closeToken === 'undefined')
-      closeToken = mapClosed();
-    result['close'] = closeToken;
-    return result;
-
-    function mapClosed() {
-      switch(openToken) {
-        case '\\(':
-          return '\\)';
-        case '\\[':
-          return '\\]';
-        case '<':
-          return '>';
-        case '{':
-        default:
-          return '}';
-      }
-    }
-  }
 }
 
 exports.Scoper = Scoper;
--- test/analyzers/scoper.js
+++ test/analyzers/scoper.js
@@ -1,60 +1,73 @@
 #!/usr/bin/env node
 
 var assert = require( 'assert' )
+  , fluke = require( 'flukejs' )
+  , Test = require( 'mucbuc-jsthree' ).Test
   , Scoper = require( '../../src/analyzers/scoper').Scoper
   , Base = require( '../base' ).Base
   , Test = require( 'mucbuc-jsthree' ).Test;
 
 assert( typeof Scoper !== 'undefined' );
 
-test( basicScope );
-test( nestedScopes );
-test( aggregateScopes );
-test( alternativeScopeTag ); 
+test( basicScope, { 'open': '{', 'close': '}' } );
+test( nestedScopes, { 'open': '{', 'close': '}' } );
+test( aggregateScopes, { 'open': '{', 'close': '}' } );
+test( alternativeScopeTag, { 'open': '<', 'close': '>' } ); 
 
-function alternativeScopeTag(emitter, parser) {
-  var rules = { 'open': '<', 'close': '>' };
-
+function alternativeScopeTag(emitter, process) {
   emitter.expect( 'open scope', 'template' );
   emitter.expect( 'close scope', 'typename' );
-  parser.process( 'template< typename >', rules );
+  process( 'template< typename >' );
 
   emitter.expect( 'open scope', 'template' );
   emitter.expect( 'close scope', 'template<typename>' );
-  parser.process( 'template< template< typename > >', rules );
+  process( 'template< template< typename > >' );
 }
 
-function aggregateScopes( emitter, parser ) {
+function aggregateScopes( emitter, process ) {
 
   emitter.expect( 'open scope', 'namespace outside' );
   emitter.expect( 'close scope', 'namespace inside1{}namespace inside2{}' );
   emitter.expect( 'end' );
-  parser.process( 'namespace outside{ namespace inside1 {} namespace inside2 {} }' );
+  process( 'namespace outside{ namespace inside1 {} namespace inside2 {} }' );
 }
 
-function nestedScopes(emitter, parser) {
+function nestedScopes(emitter, process) {
 
   emitter.expect( 'open scope', 'namespace hello' );
   emitter.expect( 'close scope', 'namespace world{namespace{}}' );
   emitter.expect( 'end' );
-  parser.process( 'namespace hello{ namespace world{ namespace {} } }' );
+  process( 'namespace hello{ namespace world{ namespace {} } }' );
 }
 
-function basicScope(emitter, parser) {
+function basicScope(emitter, process) {
 
   emitter.expect( 'open scope', 'namespace bla' );
   emitter.expect( 'close scope', '' );
-  parser.process( 'namespace bla {}' );
+  process( 'namespace bla {}' );
 
   emitter.expect( 'open scope', 'namespace bla' );
   emitter.expect( 'close scope', 'hello;' );
-  parser.process( 'namespace bla { hello; }' );
+  process( 'namespace bla { hello; }' );
 
   emitter.expect( 'open scope', 'namespace bla' );
   emitter.expect( 'close scope', 'hello' );
-  parser.process( 'namespace bla { hello }' );
+  process( 'namespace bla { hello }' );
 }
 
-function test(f) {
-  Base.test( f, Scoper );
+function test(f, rules ) {
+  var emitter = new Test.Emitter
+    , scoper = new Scoper( emitter, splitNext );
+  
+  f( emitter, splitNext );
+  process.on( 'exit', function() {
+    console.log( f.name + ' passed' );
+  });
+
+  function splitNext( code ) {
+    fluke.splitNext( code, function(type, lhs, rhs, token) {
+        emitter.emit(type, lhs, rhs, token);
+      }
+      , rules ); 
+  }
 }
